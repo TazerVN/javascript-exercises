fd2361044b0b9269ad3d1dbe886780f9e45069a9
TazerVN <157727133+TazerVN@users.noreply.github.com>
2024-11-13 21:22:54 +0700

    feat: complete from 7 to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(death, birth){
+    if (!death){
+        death = new Date().getFullYear()
+    }
+    return death - birth
+}
 
-};
+const findTheOldest = function(people) {
+    return people.reduce((personA, personB) => {
+        return (getAge(personA.yearOfDeath, personA.yearOfBirth))
+         > (getAge(personB.yearOfDeath, personB.yearOfBirth))
+        ? personA
+        : personB})
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
